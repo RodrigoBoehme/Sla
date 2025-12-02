c57322c0368e5bbfd5718c0a4eac3cb99e9eb1a7
RodrigoBoehme <robi.boehme@gmail.com>
2025-12-01 21:55:23 -0300

    Atualizado Script e index na aula9 UC12

--- UC12/aula9/js/script.js
+++ UC12/aula9/js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{// Essa linha previne o Javascript rodar antes da pagina carregar completamente, caso o <Link> no html não use/funcione o defer
     const btn_click=document.getElementById("botao-click");
+    const btnP=document.getElementById("btn-novo-paragrafo")
+    const body=document.querySelector("body");
     const paragrafo=document.getElementById("numbas");
 
     let contador=0;
@@ -8,5 +10,11 @@ document.addEventListener("DOMContentLoaded",()=>{// Essa linha previne o Javasc
         contador++;
         paragrafo.textContent=`Voce clicou ${contador} ${contador > 1 ? "vezes":"vez"}.`;
     });
+    btnP.addEventListener("click",()=>{
+        const p=document.createElement("p");
+        p.textContent=`Novo botão gerado pelo paragrafo`;
+        body.appendChild(p);
+
+    })
 
 });
